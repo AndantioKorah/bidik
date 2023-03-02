32900c12462262c57cd751523d580c0586a374f1
Youri <torehyouri@gmail.com>
2023-03-02 11:13:33 +0800

    pull

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://192.168.1.45/bidik/';
-$config['base_url'] = 'http://localhost/bidik/';
+$config['base_url'] = 'http://192.168.1.7/bidik/';
+// $config['base_url'] = 'http://localhost/bidik/';
 // 
 /*
 |--------------------------------------------------------------------------
